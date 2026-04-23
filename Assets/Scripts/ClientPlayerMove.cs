a6c0f0844a9d3853ffbf59774dfa2f94e0902e27
Turith1 <162712980+Turith1@users.noreply.github.com>
2026-04-23 17:56:09 -0300

    characters walking

---
+++ Assets/Scripts/ClientPlayerMove.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.InputSystem;
+using StarterAssets;
+using Unity.Cinemachine;
+
+public class ClientPlayerMove : NetworkBehaviour
+{
+    [SerializeField] private PlayerInput m_PlayerInput;
+    [SerializeField] private StarterAssetsInputs m_StarterAssetsInputs;
+    [SerializeField] private ThirdPersonControls m_ThirdPersonController;
+    [SerializeField] private GameObject m_cineCam;
+    [SerializeField] private GameObject _mainCamera;
+
+    private void Awake()
+    {
+        m_cineCam.SetActive(false);
+        if (_mainCamera == null)
+        {
+            _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        if (IsOwner)
+        {
+            m_cineCam.SetActive(true);
+            m_PlayerInput.enabled = true;
+            m_StarterAssetsInputs.enabled = true;
+            m_ThirdPersonController.enabled = true;
+        }
+        else if(!IsOwner && !IsServer)
+        {
+            m_PlayerInput.enabled = false;
+            m_StarterAssetsInputs.enabled = false;
+        }
+    }
+
+    [ServerRpc]
+    public void UpdateInputServerRpc(Vector2 move, Vector2 look, bool jump, bool sprint, float camRotation)
+    {
+        m_ThirdPersonController.SetServerInput(move, look, jump, sprint, camRotation);
+    }
+
+    private void Update()
+    {
+        if (!IsOwner)
+            return;
+
+        UpdateInputServerRpc(m_StarterAssetsInputs.move, m_StarterAssetsInputs.look, m_StarterAssetsInputs.jump, m_StarterAssetsInputs.sprint, _mainCamera.transform.eulerAngles.y);
+    }
+}
